befbb27689ef553a8a90908167896280a090a64c
Marcel Witoschek <marcel@marciland.de>
2022-09-10 22:15:11 +0200

    added hero class with some basic dataclasses

---
+++ src/dsa5/Dataclasses/Erfahrungsgrad.cs
@@ -0,0 +1,9 @@
+namespace dsa5.Dataclasses;
+
+public class Erfahrungsgrad
+{
+    public string Grad { get; set; }
+    public int APGesamt { get; set; }
+    public int APVerfügbar { get; set; }
+    public int APAusgegeben { get; set; }
+}
