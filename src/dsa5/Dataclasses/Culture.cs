2921fd313873d20475fa35108002735919a104d4
Marcel Witoschek <marcel@marciland.de>
2022-09-27 22:29:04 +0200

    add new culture

--- src/dsa5/Dataclasses/Culture.cs
+++ src/dsa5/Dataclasses/Culture.cs
@@ -19,7 +19,8 @@
             return new List<Culture>
             {
                 new Culture(){ name = "Andergaster", AP = 20, language = "Garethi",                socialStatus = new (){ "Adel", "Unfrei"}, skills = new(){ "Holzbearbeitung +2", "Orientierung +1", "Pflanzenkunde +1", "Sagen & Legenden +1", "Tierkunde +1", "Wildnisleben +2" } },
-                new Culture(){ name = "Aranier",     AP = 26, language = "Tulamidya oder Garethi", socialStatus = new (){ "Adel", "Unfrei"}, skills = new(){ "Betören +1", "Brett- & Glücksspiel +1", "Gassenwissen +2", "Handel +2", "Menschenkenntnis +1", "Rechnen +1", "Sagen & Legenden +1", "Stoffbearbeitung +1", "Überreden +2" } }
+                new Culture(){ name = "Aranier",     AP = 26, language = "Tulamidya oder Garethi", socialStatus = new (){ "Adel", "Unfrei"}, skills = new(){ "Betören +1", "Brett- & Glücksspiel +1", "Gassenwissen +2", "Handel +2", "Menschenkenntnis +1", "Rechnen +1", "Sagen & Legenden +1", "Stoffbearbeitung +1", "Überreden +2" } },
+                new Culture(){ name = "Bornländer",  AP = 18, language = "Garethi",                socialStatus = new (){ "Adel", "Unfrei"}, skills = new(){ "Fährtensuchen +1", "Holzbearbeitung +2", "Lebensmittelbearbeitung +1", "Orientierung +1", "Pflanzenkunde +1", "Wildnisleben +1", "Zechen +2" } }
             };
         }
 
