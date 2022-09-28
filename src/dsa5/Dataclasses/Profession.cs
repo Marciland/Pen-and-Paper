81ad16dce4778058f85f0ae94c250c836432f01f
Marcel Witoschek <marcel@marciland.de>
2022-09-28 13:55:24 +0200

    prepare step 5

--- src/dsa5/Dataclasses/Profession.cs
+++ src/dsa5/Dataclasses/Profession.cs
@@ -11,6 +11,20 @@
         public List<string> skills;
         public List<Spell> spells;
         public List<Profession> variants;
+
+        public override string ToString()
+        {
+            return $"{name} ({AP}AP)";
+        }
+
+        public static List<Profession> GetProfessions()
+        {
+            return new()
+            {
+                new Profession{}
+            };
+        }
+
     }
 
 }
--- src/dsa5/Pages/CreateHero/Step5.xaml.cs
+++ src/dsa5/Pages/CreateHero/Step5.xaml.cs
@@ -15,16 +15,18 @@ public partial class Step5 : ContentPage
         _level = level;
 		InitializeComponent();
         APBudget.Text = $"AP-Konto: {level.APAvailable}";
+        ProfessionPicker.ItemsSource = Profession.GetProfessions();
+        ProfessionPicker.SelectedIndex = 0;
     }
 
     private void Continue(object sender, EventArgs e)
     {
-
+        //create new level to pass on to step6, push step6 with selected profession, culture, species, newlevel
     }
 
     private void OnProfessionPickerSelectedIndexChanged(object sender, EventArgs e)
     {
-     
+        //fill labels with information, change apbudget.text accordingly
     }
 
     private void Back(object sender, EventArgs e)
