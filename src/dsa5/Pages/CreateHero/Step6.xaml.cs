e3a38c8542ce25169dae9cbaf549470a1fbbcf23
Marcel Witoschek <marcel@marciland.de>
2022-09-29 01:09:02 +0200

    added step 6 Co-authored-by: NigrumMortis

---
+++ src/dsa5/Pages/CreateHero/Step6.xaml.cs
@@ -0,0 +1,32 @@
+using dsa5.Dataclasses;
+
+namespace dsa5.Pages.CreateHero;
+
+public partial class Step6 : ContentPage
+{
+    private readonly Profession _profession;
+    private readonly Culture _culture;
+    private readonly Species _species;
+    private readonly Level _level;
+
+	public Step6(Profession profession, Culture culture, Species species, Level level)
+	{
+        _profession = profession;
+        _culture = culture;
+        _species = species;
+        _level = level;
+        InitializeComponent();
+        APBudget.Text = $"AP-Konto: {level.APAvailable}";
+    }
+
+    private void Continue(object sender, EventArgs e)
+    {
+        //create new level to pass on to step 7 still needs to be refined on miro
+    }
+
+    private void Back(object sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
+
+}
